71086c43bc9bd5d3110aba2bd8241b95f75174a2
Hamilton Lima <hamilton.lima@gmail.com>
2021-07-08 21:25:36 -0400

    Creates empty renderer component

---
+++ web/src/app/renderer/renderer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-renderer',
+  templateUrl: './renderer.component.html',
+  styleUrls: ['./renderer.component.scss']
+})
+export class RendererComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
