d7b1d9eddfcd634b71fb0f2e488d51d27a7cae5f
Hamilton Lima <hamilton.lima@gmail.com>
2021-07-10 17:49:18 -0400

    Simplify pickedPoint gathering when click

--- web/src/app/renderer/editor.service.ts
+++ web/src/app/renderer/editor.service.ts
@@ -15,39 +15,13 @@ import {
 })
 export class EditorService {
   setup(scene: Scene): Scene {
-    let vector: Vector3;
-    scene.onPointerDown = function (event, pickResult) {
-      if( !pickResult || !pickResult.pickedPoint){
-        console.warn('No picked result');
-        return;
-      }
 
-      //left mouse click
-      if (event.button == 0) {
-        vector = pickResult.pickedPoint;
-        console.log(
-          'left mouse click: ' + vector.x + ',' + vector.y + ',' + vector.z
-        );
+    scene.onPointerObservable.add((pointerInfo) => {
+      if( pointerInfo.pickInfo.pickedPoint){
+        console.log('pointerinfo', pointerInfo.pickInfo.pickedPoint);
       }
-      //right mouse click
-      if (event.button == 2) {
-        vector.x = pickResult.pickedPoint.x;
-        vector.y = pickResult.pickedPoint.y;
-        vector.z = pickResult.pickedPoint.z;
-        console.log(
-          'right mouse click: ' + vector.x + ',' + vector.y + ',' + vector.z
-        );
-      }
-      //Wheel button or middle button on mouse click
-      if (event.button == 1) {
-        vector['x'] = pickResult.pickedPoint['x'];
-        vector['y'] = pickResult.pickedPoint['y'];
-        vector['z'] = pickResult.pickedPoint['z'];
-        console.log(
-          'middle mouse click: ' + vector.x + ',' + vector.y + ',' + vector.z
-        );
-      }
-    };
+    });
+
     return scene;
   }
 
