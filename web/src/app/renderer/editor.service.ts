8789b7c33ca679eed9f87b5bdd6b251a445352a6
Hamilton Lima <hamilton.lima@gmail.com>
2021-07-10 18:02:36 -0400

    extract 3D coords for click

--- web/src/app/renderer/editor.service.ts
+++ web/src/app/renderer/editor.service.ts
@@ -10,15 +10,19 @@ import {
   Vector3,
 } from '@babylonjs/core';
 
+const POINTERDOWN = 'pointerdown';
+
 @Injectable({
   providedIn: 'root',
 })
 export class EditorService {
   setup(scene: Scene): Scene {
-
     scene.onPointerObservable.add((pointerInfo) => {
-      if( pointerInfo.pickInfo.pickedPoint){
-        console.log('pointerinfo', pointerInfo.pickInfo.pickedPoint);
+      if (pointerInfo.pickInfo.pickedPoint) {
+        if (pointerInfo.event.type == POINTERDOWN) {
+          console.log('pointerinfo', pointerInfo.pickInfo.pickedPoint);
+          const position = pointerInfo.pickInfo.pickedPoint;
+        }
       }
     });
 
