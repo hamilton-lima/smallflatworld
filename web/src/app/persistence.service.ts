cedd79c591596a84c25a13918a6e3055d49c322c
Hamilton Lima <hamilton.lima@gmail.com>
2021-07-10 19:05:24 -0400

    Adds pouchDB and name generator

---
+++ web/src/app/persistence.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import * as PouchDB from 'pouchdb';
+import { v4 as uuidv4 } from 'uuid';
+import { uniqueNamesGenerator, Config, adjectives, colors, animals, names } from 'unique-names-generator';
+
+const uniqueNameConfig: Config = {
+  dictionaries: [adjectives, colors, animals, names],
+  length: 3
+}
+
+// const REALM = 'realm';
+// const ENVIRONMENT = 'environment';
+
+// export class Realm {
+//   id: string;
+//   name: string;
+// }
+
+// export class RealmList {
+
+// }
+
+// export class Environment {
+//   currentRealm: string;
+// }
+
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class PersistenceService {
+//   db: PouchDB.Database;
+
+//   constructor() {
+//     this.db = new PouchDB('smallflatworld');
+//   }
+
+//   getRealms(): Promise<Realm[]> {
+//     return this.db.get(REALM);
+//   }
+  
+//   updateRealms(realms: Realm[]) {
+//     return this.db.put(REALM, realms);
+//   }
+  
+//   getEnvironment(): Promise<Environment> {
+//     return this.db.get(ENVIRONMENT);
+//   }
+
+//   addRealm(): Promise<Realm> {
+//     this.getRealms().then( (realms: Realm[])=>{
+//       const realm = new Realm();
+//       realm.id = uuidv4();
+//       realm.name = uniqueNamesGenerator(uniqueNameConfig);
+//       realms.push(realm);
+//       return this.updateRealms(realms);
+//     })
+//   }
+
+  
+}
