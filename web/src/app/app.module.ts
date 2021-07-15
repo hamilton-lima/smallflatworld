294b73a773b9179a9cdacb298a050c584babf16b
Hamilton Lima <hamilton.lima@gmail.com>
2021-07-14 21:23:41 -0400

    Adds angular material

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RendererComponent } from './renderer/renderer.component';
 import { LogLevel, NgxFancyLoggerModule } from 'ngx-fancy-logger';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, RendererComponent],
@@ -21,6 +22,7 @@ import { LogLevel, NgxFancyLoggerModule } from 'ngx-fancy-logger';
         [LogLevel.ERROR]: 'SFW 💀',
       },
     }),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
