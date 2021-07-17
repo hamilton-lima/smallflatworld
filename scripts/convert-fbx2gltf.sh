57223ca9708e132079fca15bf087452a9f92dca4
Hamilton Lima <hamilton.lima@gmail.com>
2021-07-16 20:17:36 -0400

    Adds script to convert fbx to gltf

---
+++ scripts/convert-fbx2gltf.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://github.com/facebookincubator/FBX2glTF/releases/download/v0.9.7/FBX2glTF-linux-x64 -o FBX2glTF
+chmod +x FBX2glTF-linux-x64
+./FBX2glTF-linux-x64 --binary --draco --verbose \
+          --input  ../web/src/assets/character/model/characterMedium.fbx \
+          --output  ../web/src/assets/character/model/characterMedium.glb
