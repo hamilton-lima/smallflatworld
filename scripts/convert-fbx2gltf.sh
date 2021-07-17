fcdc76b9700cfefae593e2fecefb2bf0e1ed67c3
Hamilton Lima <hamilton.lima@gmail.com>
2021-07-16 21:29:06 -0400

    Include animations in the conversion script

--- scripts/convert-fbx2gltf.sh
+++ scripts/convert-fbx2gltf.sh
@@ -2,7 +2,21 @@
 
 wget https://github.com/facebookincubator/FBX2glTF/releases/download/v0.9.7/FBX2glTF-linux-x64
 chmod +x FBX2glTF-linux-x64
+
 ./FBX2glTF-linux-x64 --binary --draco --verbose --no-flip-v \
           --input  ../web/src/assets/character/model/characterMedium.fbx \
           --output  ../web/src/assets/character/model/characterMedium.glb
+
+./FBX2glTF-linux-x64 --binary --draco --verbose --no-flip-v \
+          --input  ../web/src/assets/character/animations/idle.fbx \
+          --output  ../web/src/assets/character/animations/idle.glb
+
+./FBX2glTF-linux-x64 --binary --draco --verbose --no-flip-v \
+          --input  ../web/src/assets/character/animations/jump.fbx \
+          --output  ../web/src/assets/character/animations/jump.glb
+
+./FBX2glTF-linux-x64 --binary --draco --verbose --no-flip-v \
+          --input  ../web/src/assets/character/animations/run.fbx \
+          --output  ../web/src/assets/character/animations/run.glb
+
 rm FBX2glTF-linux-x64          
