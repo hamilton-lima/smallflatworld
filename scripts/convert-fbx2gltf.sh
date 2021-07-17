010e321e74062f9d9a4bd8e23c301eb3b9e0ad95
Hamilton Lima <hamilton.lima@gmail.com>
2021-07-16 21:16:28 -0400

    Updates conversion script to prevent flip-V

--- scripts/convert-fbx2gltf.sh
+++ scripts/convert-fbx2gltf.sh
@@ -2,7 +2,7 @@
 
 wget https://github.com/facebookincubator/FBX2glTF/releases/download/v0.9.7/FBX2glTF-linux-x64
 chmod +x FBX2glTF-linux-x64
-./FBX2glTF-linux-x64 --binary --draco --verbose \
+./FBX2glTF-linux-x64 --binary --draco --verbose --no-flip-v \
           --input  ../web/src/assets/character/model/characterMedium.fbx \
           --output  ../web/src/assets/character/model/characterMedium.glb
 rm FBX2glTF-linux-x64          
