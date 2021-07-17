dbdee45ee63217f1dac30c889edfdd56e09a171f
Hamilton Lima <hamilton.lima@gmail.com>
2021-07-16 20:20:57 -0400

    Fixes FBX conversion script

--- scripts/convert-fbx2gltf.sh
+++ scripts/convert-fbx2gltf.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-wget https://github.com/facebookincubator/FBX2glTF/releases/download/v0.9.7/FBX2glTF-linux-x64 -o FBX2glTF
+wget https://github.com/facebookincubator/FBX2glTF/releases/download/v0.9.7/FBX2glTF-linux-x64
 chmod +x FBX2glTF-linux-x64
 ./FBX2glTF-linux-x64 --binary --draco --verbose \
           --input  ../web/src/assets/character/model/characterMedium.fbx \
           --output  ../web/src/assets/character/model/characterMedium.glb
+rm FBX2glTF-linux-x64          
