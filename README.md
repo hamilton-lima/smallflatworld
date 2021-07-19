6afe60124e5516baddc3b9653ad352f74eb92b2d
Hamilton Lima <hamilton.lima@gmail.com>
2021-07-19 07:58:25 -0400

    Create first empty page

--- README.md
+++
@@ -1,2 +0,0 @@
-# smallflatworld
-web 3D sandbox
--- web/src/app/renderer/renderer.model.ts
+++
@@ -1,9 +0,0 @@
-import { Engine, Camera, Scene, HemisphericLight, Mesh } from '@babylonjs/core';
-
-export class EngineState {
-  engine: Engine;
-  scene: Scene;
-  camera: Camera;
-  mainLight: HemisphericLight;
-  character: Mesh;
-}
