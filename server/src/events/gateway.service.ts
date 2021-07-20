bf4522332290f4c01e163ee5d8f1ed5fcfd442bd
Hamilton Lima <hamilton.lima@gmail.com>
2021-07-20 00:02:25 -0400

    Sending and receiving message thru websocket

---
+++ server/src/events/gateway.service.ts
@@ -0,0 +1,28 @@
+import {
+    ConnectedSocket,
+    MessageBody,
+    SubscribeMessage,
+    WebSocketGateway,
+    WebSocketServer,
+  } from '@nestjs/websockets';
+  import { Server, Socket } from 'ws';
+  
+  export class ShareResponse {
+      realmKey: string;
+  }
+
+  @WebSocketGateway(8080)
+  export class GatewayService {
+    @WebSocketServer()
+    server: Server;
+  
+    @SubscribeMessage('events')
+    handleEvent(
+      @MessageBody() data: string,
+      @ConnectedSocket() client: Socket,
+    ): string {
+        console.log('message received', data);
+        return data + '-echo';
+    }
+
+  }
