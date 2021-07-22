c9b45212a977b57cc0f28b0485f1312bc7a2f55d
Hamilton Lima <hamilton.lima@gmail.com>
2021-07-21 22:53:32 -0400

    Store SceneElement in memory

--- server/src/memory.storage.ts
+++ server/src/memory.storage.ts
@@ -1,20 +1,34 @@
-export class RealmData {}
+import { SceneElement, StateUpdate } from "./events.model";
+
+class StateUpdateCached {
+  map: Map<string, SceneElement>;
+
+  constructor() {
+    this.map = new Map<string, SceneElement>();
+  }
+}
 
 export class MemoryStorage {
-  data: Map<string, RealmData> = new Map<string, RealmData>();
+  data: Map<string, StateUpdateCached> = new Map<string, StateUpdateCached>();
+
   addRealm(uuid: string) {
     if (this.data.has(uuid)) {
       throw new Error("uuid already in memory");
     } else {
-      this.data.set(uuid, new RealmData());
+      this.data.set(uuid, new StateUpdateCached());
     }
   }
 
-  getRealm(uuid: string): RealmData {
+  getRealm(uuid: string): StateUpdate {
     if (!this.data.has(uuid)) {
       throw new Error("uuid dont exist in memory");
     } else {
-      return this.data.get(uuid);
+      const data = this.data.get(uuid);
+      const array = Array.from(data.map.values());
+      const result = <StateUpdate>{
+        data: array,
+      };
+      return result;
     }
   }
 }
