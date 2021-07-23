07e4b3d0fafd3e5c1b610ea304d57d3027527664
Hamilton Lima <hamilton.lima@gmail.com>
2021-07-22 23:07:38 -0400

    Removes servernestJs

--- server-nestjs/README.md
+++
@@ -1,6 +0,0 @@
-# small flat world server
-
-Implementation notes 
-
-send data in the format
- { "event": "events", "data": "test" }
--- server-nestjs/src/events/events.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GatewayService } from './gateway.service';
-
-@Module({
-  providers: [GatewayService]
-})
-export class EventsModule {}
