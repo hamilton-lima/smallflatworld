58bf4987401fb2779b8204ab4a96fbabf43c1ecd
Hamilton Lima <hamilton.lima@gmail.com>
2021-08-01 12:33:42 -0400

    Fixes double stringify

--- server/src/ping.handler.ts
+++ server/src/ping.handler.ts
@@ -3,10 +3,9 @@ import { Handler } from "./handler.interface";
 import { EventsHandler } from "./events.handler";
 
 export class PingHandler implements Handler {
-  readonly result = JSON.stringify(<PingResponse>{ data: "pong" });
+  readonly result = <PingResponse>{ data: "pong" };
 
   handle(request: PingRequest, parent: EventsHandler): void {
-    console.log("ping");
     parent.send(Actions.Ping, this.result);
   }
 }
