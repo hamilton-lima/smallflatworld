310d7cafac8a9c48abe3bfdd5981a2a1a3c1633c
Hamilton Lima <hamilton.lima@gmail.com>
2021-08-24 20:55:31 -0400

    Atempt to fix unit tests

--- web/src/app/realm/realm-upload/realm-upload.component.spec.ts
+++ web/src/app/realm/realm-upload/realm-upload.component.spec.ts
@@ -1,25 +1,24 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RealmUploadComponent } from './realm-upload.component';
+import { AppModule } from '../../app.module';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('RealmUploadComponent', () => {
   let component: RealmUploadComponent;
   let fixture: ComponentFixture<RealmUploadComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ RealmUploadComponent ]
-    })
-    .compileComponents();
-  }));
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, AppModule],
+      declarations: [RealmUploadComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(RealmUploadComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  fit('should create', () => {
     expect(component).toBeTruthy();
   });
 });
--- web/src/app/renderer/renderer.component.spec.ts
+++ web/src/app/renderer/renderer.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppModule } from '../app.module';
 
 import { RendererComponent } from './renderer.component';
 
@@ -6,12 +8,13 @@ describe('RendererComponent', () => {
   let component: RendererComponent;
   let fixture: ComponentFixture<RendererComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ RendererComponent ]
+      declarations: [ RendererComponent ],
+      imports: [RouterTestingModule, AppModule],
     })
     .compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(RendererComponent);
