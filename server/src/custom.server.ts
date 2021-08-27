8a59a68a5652cc3dbdac41f05c067797cbbe42db
Hamilton Lima <hamilton.lima@gmail.com>
2021-08-27 08:35:13 -0400

    Adds extra log to server

--- server/src/custom.server.ts
+++ server/src/custom.server.ts
@@ -4,14 +4,17 @@ import { EventsHandler } from './events.handler';
 export class CustomServer {
   constructor(){
     const server = new WebSocket.Server({ port: 8080 });
+    console.log('server started at port 8080');
 
     server.on('connection', function connection(ws:WebSocket) {
+      console.log('new connection');
       const handler = new EventsHandler(ws);
       ws.on('message', function incoming(message:WebSocket.Data) {
         handler.onMessage(message);
       });
 
       ws.on('close', function incoming(message:WebSocket.Data) {
+        console.log('connection closed');
         handler.onClose();
       });
 
