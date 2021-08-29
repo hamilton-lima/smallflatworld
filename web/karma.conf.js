7313ae52a4faa61022284fdc26d864f6ed75223b
Hamilton Lima <hamilton.lima@gmail.com>
2021-08-29 15:52:55 -0400

    Adds code coverage report

--- web/karma.conf.js
+++ web/karma.conf.js
@@ -19,11 +19,11 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/web'),
-      reports: ['html', 'lcovonly', 'text-summary', 'coverage'],
+      dir: require('path').join(__dirname, './coverage'),
+      reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml', 'spec'],
+    reporters: ['progress', 'kjhtml', 'spec', 'coverage-istanbul'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
