5a533e51a67c5533b76b6dc46f159ac87da97bf6
Hamilton Lima <hamilton.lima@gmail.com>
2021-09-21 22:54:51 -0400

    Loads components by componentID (Broken)

--- web/src/app/editor/editor-library.service.ts
+++ web/src/app/editor/editor-library.service.ts
@@ -1,14 +1,73 @@
 import { Injectable } from '@angular/core';
-import { Library } from './editor-library.model';
-import { kenneyLibrary} from './kenney.library';
+import { AbstractMesh, Scene } from '@babylonjs/core';
+import { MeshLoaderService } from '../mesh/mesh-loader.service';
+import { MeshService } from '../renderer/mesh.service';
+import {
+  Library,
+  LibraryComponent,
+  PRIMITIVE_COMPONENT,
+} from './editor-library.model';
+import { kenneyLibrary } from './kenney.library';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EditorLibraryService {
-  constructor() {}
+  private cache: Map<string, AbstractMesh> = new Map();
+  private components: Map<string, LibraryComponent> = new Map();
+
+  constructor(private loader: MeshLoaderService, private mesh: MeshService) {
+    this.setComponentNames();
+  }
+
+  setComponentNames() {
+    this.components.set(PRIMITIVE_COMPONENT.id, PRIMITIVE_COMPONENT);
+
+    kenneyLibrary.forEach((library) => {
+      library.components.forEach((component) => {
+        this.components.set(component.id, component);
+      });
+    });
+  }
 
   getLibraries(): Library[] {
     return kenneyLibrary;
+  }
+
+  getComponent(componentID: string) {
+    if (this.components.has(componentID)) {
+      return this.components.get(componentID);
+    } else {
+      console.warn('componentID not found', componentID);
+      // unknow componentID
+      return PRIMITIVE_COMPONENT;
+    }
+  }
+
+  // get Mesh based on componentID
+  async getMesh(scene: Scene, componentID: string): Promise<AbstractMesh> {
+    const component = this.getComponent(componentID);
+
+    let result = null;
+    if (this.cache.has(component.id)) {
+      result = this.cache.get(component.id);
+    } else {
+      // if primitive dont load, create the primitive box
+      if (component.id == PRIMITIVE_COMPONENT.id) {
+        result = this.mesh.getBox(scene);
+      } else {
+        // not present in the cache load the model
+        result = await this.loader.load(
+          scene,
+          component.model3D,
+          component.name,
+          false
+        );
+      }
+
+      this.cache.set(component.id, result);
+    }
+
+    return result;
   }
 }
