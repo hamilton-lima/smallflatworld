cf6186881b9164e237648c3e80acf56ae43f7a5c
Hamilton Lima <hamilton.lima@gmail.com>
2021-09-24 19:58:23 -0400

    Adjusts light on scenario

--- web/src/app/renderer/renderer.service.ts
+++ web/src/app/renderer/renderer.service.ts
@@ -5,6 +5,8 @@ import {
   Color4,
   Vector3,
   HemisphericLight,
+  DirectionalLight,
+  Color3,
 } from '@babylonjs/core';
 import { Subject } from 'rxjs';
 import { EditorService } from '../editor/editor.service';
@@ -26,13 +28,43 @@ export class RendererService {
     result.scene.useRightHandedSystem = true;
     result.scene.clearColor = new Color4(0, 0, 0, 0);
 
-    result.mainLight = new HemisphericLight(
-      'light1',
+    // result.mainLight = new HemisphericLight(
+    //   'light1',
+    //   new Vector3(0, 1, 0),
+    //   result.scene
+    // );
+
+    const ambient = new HemisphericLight(
+      'ambient',
       new Vector3(0, 1, 0),
       result.scene
     );
+    ambient.diffuse = new Color3(1, 1, 1);
+    ambient.specular = new Color3(0.4, 0.4, 0.4);
+    ambient.groundColor = new Color3(1, 1, 1);
+    ambient.intensity = 0.9;
+
+    const light = new DirectionalLight(
+      'light',
+      new Vector3(0, -1, 0),
+      result.scene
+    );
+    this.setLightPositionByAngle(light, 120, 50, 100);
+    light.autoUpdateExtends = true;
+    light.diffuse = new Color3(1, 1, 1);
+    light.intensity = 1;
 
     return result;
+  }
+
+  // from https://github.com/nimadez/voxel-builder/blob/353ce8b56f1c57aaa6e132a6907f62d175936809/index.html#L1517
+  setLightPositionByAngle(light, angle, distance, height) {
+    const x = Math.cos((angle * Math.PI) / 180) * distance;
+    const y = height;
+    const z = Math.sin((angle * Math.PI) / 180) * distance;
+    const pos = new Vector3(x, y, z);
+    light.position = pos; // our primary shadow light
+    light.setDirectionToTarget(Vector3.Zero());
   }
 
   update(state: EngineState, elements: SceneElement[]) {
