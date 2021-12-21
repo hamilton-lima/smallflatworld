12901a21fa0adddeccfa41a219aaa05b4e4f8587
Hamilton Lima <hamilton.lima@gmail.com>
2021-12-20 20:55:18 -0500

    Moves connect animation to nodes to the mesh loader

--- web/src/app/mesh/character.service.ts
+++ web/src/app/mesh/character.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Mesh, SceneLoader, StandardMaterial, Texture } from '@babylonjs/core';
+import { ISceneLoaderAsyncResult, Mesh, SceneLoader, StandardMaterial, Texture } from '@babylonjs/core';
 import { CharacterAnimations, EngineState } from '../renderer/renderer.model';
 import { MeshLoaderService } from './mesh-loader.service';
 
@@ -32,53 +32,24 @@ export class CharacterService {
   }
 
   async loadAnimated(engineState: EngineState): Promise<CharacterAndAnimations> {
-    const loaded = await SceneLoader.ImportMeshAsync(
+    const loaded: ISceneLoaderAsyncResult = await SceneLoader.ImportMeshAsync(
       '',
       'assets/',
       'library/kaykit/kayyit-dungeon-pack-1/characters/character_barbarian2.glb',
       engineState.scene
     );
 
-    const animations = await this.loader.loadAllAnimations(
-      engineState.scene,
+    const jump = await this.loader.loadAllAnimation(
+      engineState.scene, loaded,
       'library/kaykit/kayyit-dungeon-pack-1/animations/jump3.glb',
       true
     );
 
-    const cheer = animations[0];
-    console.log('(1) animations', cheer.targetedAnimations);
     console.log('(1) loaded', loaded);
 
     const result = <CharacterAndAnimations>{
       character: <Mesh>loaded.meshes[0],
-      animations: <CharacterAnimations>{}
-    }
-
-    if (cheer) {
-      console.log('clone animation');
-      // oldTarget represents the name of the bone in the imported animation group
-      // clone will use the provided function to search for the bone in the target mesh skeleton
-      // and target the cloned animationGroup to the new bones.
-      // the function will be called for each animation
-      const newRun = cheer.clone('new-animation', (oldTarget) => {
-        let target = oldTarget;
-        console.log('search oldTarget', oldTarget.id, oldTarget);
-        // search for all nodes in the tranform nodes
-        for (let node of loaded.transformNodes) {
-          console.log('node', node.id);
-          if (node.id.toLowerCase().includes(oldTarget.id.toLowerCase())) {
-            console.log('found', node.id);
-            target = node;
-            break;
-          }
-        }
-
-        return target;
-      });
-
-      console.log('cloned animation', newRun);
-      result.animations.jump = newRun;
-      // newRun.start(true);
+      animations: <CharacterAnimations>{ jump: jump }
     }
 
     return result;
