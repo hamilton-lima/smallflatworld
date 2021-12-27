a3be77003d5e25c3e8a58a994e5a48d0b6af47fe
Hamilton Lima <hamilton.lima@gmail.com>
2021-12-26 20:05:19 -0500

    Allow user to move camera

--- web/src/app/renderer/camera.service.ts
+++ web/src/app/renderer/camera.service.ts
@@ -1,5 +1,14 @@
 import { Injectable } from '@angular/core';
-import { Mesh, Camera, ArcFollowCamera } from '@babylonjs/core';
+import {
+  Mesh,
+  Camera,
+  ArcFollowCamera,
+  FreeCamera,
+  ArcRotateCamera,
+  Tools,
+  ArcRotateCameraPointersInput,
+  Vector3,
+} from '@babylonjs/core';
 import { EngineState } from './renderer.model';
 const DEFAULT_DISTANCE = -25;
 const DEFAULT_HEIGHT = 25;
@@ -14,22 +23,43 @@ const MAX = 70;
 export class CameraService {
   distance = DEFAULT_DISTANCE;
   height = DEFAULT_HEIGHT;
-  camera: ArcFollowCamera;
+  camera: ArcRotateCamera;
 
   setup(engineState: EngineState): Camera {
     const scene = engineState.scene;
     const character = engineState.character;
 
-    const camera = new ArcFollowCamera(
-      'mainCamera',
-      45,
-      -45,
+    // const camera = new ArcFollowCamera(
+    //   'mainCamera',
+    //   45,
+    //   -45,
+    //   this.distance,
+    //   engineState.character,
+    //   scene
+    // );
+
+    const alpha = Tools.ToRadians(135);
+    const beta = Tools.ToRadians(135);
+
+    const camera = new ArcRotateCamera(
+      'camera',
+      alpha,
+      beta,
       this.distance,
-      engineState.character,
+      engineState.character.position,
       scene
     );
+
     camera.attachControl(engineState.engine.getRenderingCanvas(), true);
-    camera.radius = this.distance;
+    camera.inputs.remove(camera.inputs.attached.keyboard);
+    // camera.panningSensibility = 0;
+
+    //enable only first mouse button
+    const mouseInput = (camera.inputs.attached.pointers as ArcRotateCameraPointersInput);
+    mouseInput.buttons = [0];
+
+    camera.collisionRadius = Vector3.One();
+    camera.checkCollisions = true; 
 
     this.camera = camera;
 
@@ -42,15 +72,15 @@ export class CameraService {
   }
 
   zoomOut() {
-    if (Math.abs(this.camera.radius) < MAX) {
-      this.camera.radius -= STEP;
-    }
+    // if (Math.abs(this.camera.radius) < MAX) {
+    //   this.camera.radius -= STEP;
+    // }
   }
 
   zoomIn() {
-    if (Math.abs(this.camera.radius) > MIN) {
-      this.camera.radius += STEP;
-    }
+    // if (Math.abs(this.camera.radius) > MIN) {
+    //   this.camera.radius += STEP;
+    // }
   }
 
   constructor() {}
